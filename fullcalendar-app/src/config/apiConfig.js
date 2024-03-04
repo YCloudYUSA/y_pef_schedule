4f6eb90ddab83d99bc858e7eaf76fdfe2796d8ba
Roman Salo <frs.daddy@gmail.com>
2024-03-04 15:03:41 +0200

    Unhardcode API_BASE_URL in the apiCnfig.js file

--- fullcalendar-app/src/config/apiConfig.js
+++ fullcalendar-app/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // apiConfig.js
-// TODO: The hardcode must be removed.
-const API_BASE_URL = 'https://yusaopeny.docksal.site';
+
+const API_BASE_URL = window.location.origin;
 const SCHEDULES_ENDPOINT = '/schedules/get-event-data-date-range';
 const UPDATE_EVENT_ENDPOINT = '/admin/openy/branch-schedules/update-event';
 
